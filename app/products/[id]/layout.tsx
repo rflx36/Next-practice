37638fe7eadae4a98859dd6750ad5d62fe1b6792
RFLX36 <rolandfonzlamoste3608@gmail.com>
2025-02-03 03:18:51 +0800

    basic routing

---
+++ app/products/[id]/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function ProductLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div>
+            {children}
+            <div>
+                <h2>Featured products section</h2>
+            </div>
+        </div>
+    )
+}
